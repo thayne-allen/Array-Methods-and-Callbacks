b024db77849caf58e967c75ba52ca1570405f5c4
thayne-allen <thayne.allen@gmail.com>
2020-06-04 22:08:41 -0600

    initial commit task 1 complete

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import { fifaData } from './fifa.js';
-console.log(fifaData);
+// console.log(fifaData);
 
-console.log('its working');
+// console.log('its working');
 // ⚽️ M  V P ⚽️ //
 
 /* Task 1: Investigate the data above. Practice accessing data by console.log-ing the following pieces of data 
@@ -12,7 +12,12 @@ console.log('its working');
 (d) Away Team goals for 2014 world cup final
 (e) Winner of 2014 world cup final */
 
-
+console.log(fifaData[828])
+console.log(fifaData[828]["Home Team Name"])
+console.log(fifaData[828]["Away Team Name"])
+console.log(fifaData[828]["Home Team Goals"])
+console.log(fifaData[828]["Away Team Goals"])
+console.log(fifaData[828]["Win conditions"])
 /* Task 2: Create a function called  getFinals that takes `data` as an argument and returns an array of objects with only finals data */
 
 function getFinals(/* code here */) {
